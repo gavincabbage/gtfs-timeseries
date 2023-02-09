d6bd85db0ba024db4a57f9c2c2620b22d105ebc6
Gavin Cabbage <dev@gavincabbage.com>
2023-02-09 09:50:55 -0500

    timescaledb

--- export/export.py
+++ export/export.py
@@ -1,21 +1,24 @@
 from flightsql import connect, FlightSQLClient
 from geojson import LineString,Feature,FeatureCollection,dump
 import os
+import psycopg2
 
-
-query = '''
-SELECT vehicle_id, time, route_id, latitude, longitude
-FROM iox.position
-WHERE LOWER(route_id) IN ('blue', 'red', 'orange', 'green-b', 'green-c', 'green-d', 'green-e', 'mattapan')
-AND time > now() - INTERVAL '1 hour'
-GROUP BY vehicle_id, time, route_id, latitude, longitude
-ORDER BY vehicle_id, time, route_id, latitude, longitude
-'''
 
 ndx_vehicle_id, ndx_time, ndx_route_id, ndx_latitude, ndx_longitude = 0, 1, 2, 3, 4
 
 
-def to_geojson(conn):
+def format_query(table_name, interval):
+    return '''
+SELECT vehicle_id, time, route_id, latitude, longitude
+FROM {table_name}
+WHERE LOWER(route_id) IN ('blue', 'red', 'orange', 'green-b', 'green-c', 'green-d', 'green-e', 'mattapan')
+AND time > now() - INTERVAL '{interval}'
+GROUP BY vehicle_id, time, route_id, latitude, longitude
+ORDER BY vehicle_id, time, route_id, latitude, longitude
+'''.format(table_name=table_name, interval=interval)
+
+
+def to_geojson(conn, query):
     cursor = conn.cursor()
 
     features = []
@@ -23,7 +26,10 @@ def to_geojson(conn):
     current_vehicle = ''
     current_route = ''
     previous_coord = ()
-    for row in cursor.execute(query):
+    print(query)
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    for row in rows: #$cursor.execute(query):
         if row[ndx_vehicle_id] != current_vehicle and current_vehicle != '':
             features.append(Feature(
                 geometry=LineString(coords),
@@ -63,7 +69,7 @@ def filter_outlier(previous, current):
     return abs(current[0]-previous[0]) > threshold or abs(current[1]-previous[1]) > threshold
 
 
-def run(host, token, bucket):
+def run_flightsql(host, token, bucket):
     client = FlightSQLClient(
         host=host,
         port=443,
@@ -72,12 +78,24 @@ def run(host, token, bucket):
     )
     conn = connect(client)
 
-    with open('mbta.geojson', 'w') as f:
-        dump(to_geojson(conn), f)
+    out = to_geojson(conn, format_query('iox.position', '1 hour'))
+    with open('mbta_flightsql.geojson', 'w') as f:
+        dump(out, f)
+
+
+def run_postgresql(dsn):
+    conn = psycopg2.connect(dsn)
+
+    out = to_geojson(conn, format_query('position', '1 hour'))
+    with open('mbta_postgresql.geojson', 'w') as f:
+        dump(out, f)
 
 
 if __name__ == '__main__':
     host = os.getenv('INFLUXDB_HOST')
     token = os.getenv('INFLUXDB_TOKEN')
     bucket = os.getenv('INFLUXDB_BUCKET')
-    run(host, token, bucket)
+    run_flightsql(host, token, bucket)
+
+    dsn = os.getenv("TIMESCALE_DSN")
+    run_postgresql(dsn)
